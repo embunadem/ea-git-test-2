8a5ffa55de148648dcfe348c2e827e1617eef993
embunadem <handsomehilmy@gmail.com>
2019-12-03 06:30:01 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,5 +11,9 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
     Consequuntur asperiores reiciendis debitis magni? Reiciendis numquam ab cumque voluptatibus quisquam. 
     Officia non veniam, doloribus saepe quo tempora ducimus sit cumque est!</p>
+    <ul>
+       <li>satu</li>
+       <li>dua</li> 
+    </ul>
 </body>
 </html>
